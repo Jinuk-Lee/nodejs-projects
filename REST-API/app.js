8e3d03709d819fdf588bfa689d262afff6a503a0
Jinuk Lee <20181680@edu.hanbat.ac.kr>
2022-06-02 15:33:17 +0900

    loginlist 설정 오류

--- REST-API/app.js
+++ REST-API/app.js
@@ -10,10 +10,9 @@ app.use(express.urlencoded({
 
 
 //localhost:3000/signup
-//회원가입 구현
 app.post('/signup', (req,
                      res) => {
-    console.log("POST /SignUp");
+    console.log("POST /signup");
 
     fs.readFile('./members.json', 'utf8',
         (error, jsonFile) => {
@@ -25,16 +24,16 @@ app.post('/signup', (req,
 
             for (let idx = 0; idx < jsonData.length; idx++) {
                 const member = jsonData[idx];
-                    if(member.identification===identification_number) {  //1인 1계정제한 주민번호 비교
-                        console.log("SignUp Failed - already exists");
-                        return res.status(404).send("SignUp Failed - already exists");
-                    }
+                if (member.identification === identification_number) {  //1인 1계정제한 주민번호 비교
+                    console.log("SignUp Failed - already exists");
+                    return res.status(404).send("SignUp Failed - already exists");
+                }
 
             }
 
             jsonData.push(req.body);
 
-            fs.writeFile('./members.json', JSON.stringify(jsonData,null,4),
+            fs.writeFile('./members.json', JSON.stringify(jsonData, null, 4),
                 "utf8", (err) => {
 
                     if (error) return console.log(error);
@@ -46,54 +45,88 @@ app.post('/signup', (req,
 //localhost:3000/login
 app.post('/login', (req,
                     res) => {
-    console.log("POST /Login");
+    console.log("POST /login");
 
     fs.readFile('./members.json', 'utf8',
         (error, jsonFile) => {
             if (error) return console.log(error);
             const jsonData = JSON.parse(jsonFile); //members.json을 string형으로 변환하여 jsonData에 저장
 
-            const members = jsonData.members; //members.json에서 members를 members변수에 저장
-            const {id, password} = req.body;
+            const {email, password} = req.body;
 
-            for (let idx = 0; idx < members.length; idx++) {
-                const member = members[idx];
-                if (member.id === id) {                //로그인 시 name이 일치하면
-                    if (member.password === password) {    //로그인 시 password가 일치하면
-                        console.log("Login Success");
-                        return res.status(200).send("Login Success"); //로그인 성공
-                    }
+            for (let idx = 0; idx < jsonData.length; idx++) {
+                const member = jsonData[idx];
+                if ((member.email === email) && (member.password === password)) {                //로그인 시 name이 일치하면
+                    console.log("Login Success");
+                    fs.readFile('./loginlist.json','utf8',
+                        (error,loginFile)=>{
+                            if (error) return console.log(error);
+
+                            const loginData = JSON.parse(loginFile);
+
+                            const newloginData = {  //배열의 이름
+                                email: req.body.email
+                            };
+
+                            loginData.push(newloginData);
+
+                            fs.writeFile('./loginlist.json', JSON.stringify(loginData, null, 4),
+                                "utf8", (err) =>{
+
+                                    if(error) return console.log(error);
+                                    return res.status(200).send("Login Success"); //로그인 성공
+                            });
+                    });
+
                 }
             }
-            console.log("Login failed");
-            res.status(404).send("Login failed"); //둘 중 하나라도 틀리면 로그인 실패
-        });
+         console.log("Login Failed");
+         res.status(404).send("Login Failed"); //둘 중 하나라도 틀리면 로그인 실패
+    });
 });
+
 //localhost:3000/reviews
 app.post('/reviews', (req
-    ,res )=> {
+    , res) => {
     console.log("POST /reviews");
 
-    fs.readFile('./reviews.json', 'utf8',
-        (error, jsonFile) => {
-            if (error) return console.log(error);
+    fs.readFile('./loginlist.json', 'utf8',
+        (err, loginFile) => {
+            if (err) return console.log(err);
+            const loginData = JSON.parse(loginFile); //members.json을 string형으로 변환하여 jsonData에 저장
 
-            const jsonData = JSON.parse(jsonFile); //members.json을 string형으로 변환하여 jsonData에 저장
-            //string("") -> json 변경
+            const email = req.body.email;
 
-            const reviews = jsonData.reviews;
+            for (let i = 0; i < loginData.length; i++) {
+                const loginEmail = loginData[i];
+                if (email === loginEmail.email) {
+                    fs.readFile('./reviews.json', 'utf8',
+                        (error, reviewsFile) => {
+                            if (error) return console.log(error);
 
-            reviews.push(req.body);
+                            const reviewData = JSON.parse(reviewsFile); //members.json을 string형으로 변환하여 jsonData에 저장
 
+                            const newReviewData = {
+                                star_ratings: req.body.star_ratings,
+                                writer: req.body.writer,
+                                comments: req.body.comments
+                            };
 
-            fs.writeFile('./reviews.json', JSON.stringify(reviews,null, 4),
-                "utf8", (error) => {
+                            reviewData.push(newReviewData);
 
-                    if (error) return console.log(error);
-                    console.log("review upload");
-                    res.status(200).send("review upload");
-                });
-        });
+                            fs.writeFile('./reviews.json', JSON.stringify(reviewData, null, 4),
+                                "utf8", (error) => {
+
+                                    if (error) return console.log(error);
+                                    return res.status(200).send("Review Upload");
+                            });
+                    });
+
+                }
+            }
+        console.log("Review Upload Failed");
+        res.status(404).send("Review Upload Faliled");
+    });
 });
 
 app.get('/reviews', (req,
